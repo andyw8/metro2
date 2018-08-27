a8691fe9835cc7d019f5c906302eb9092e5bfea8
Andy Waite <andyw8@users.noreply.github.com>
2018-08-27 14:40:46 -0400

    Delete sample test

--- spec/metro2_spec.rb
+++ spec/metro2_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe Metro2 do
   it "has a version number" do
     expect(Metro2::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
